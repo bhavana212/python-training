26b0df018b55d873e58b1beec67effc3c2100a9f
unknown <bhavanaparnasala777@gmail.com>
2021-09-28 19:15:28 +0530

    uploaded numbers.py file

---
+++ numbers.py
@@ -0,0 +1,10 @@
+n1=int(input("enter first number :"))
+n2=int(input("enter second number :"))
+n3=int(input("enter third number :"))
+if (n1 > n2) and (n1 > n3):1
+    largest = n1
+elif (n2 > n2) and (n2 > n3):
+    largest = n2
+else :
+    largest = n3
+print("the largest number is" ,largest)
