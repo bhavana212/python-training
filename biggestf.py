b3efe68b78df805d2d7ba249b9ea8e2261bb79b2
unknown <bhavanaparnasala777@gmail.com>
2021-09-29 13:50:19 +0530

    uploaded biggestf.py filebiggestf.py

---
+++ biggestf.py
@@ -0,0 +1,13 @@
+n1=int(input("enter first number :"))
+n2=int(input("enter second number :"))
+n3=int(input("enter third number :"))
+def biggest(n1,n2,n3):
+    if (n1 >= n2) and (n1 >= n3):
+         largest = n1
+    elif (n2 >= n1) and (n2 >= n3):
+         largest = n2
+    else:
+         largest = n3
+    return largest
+
+print(biggest(10,20,30))
